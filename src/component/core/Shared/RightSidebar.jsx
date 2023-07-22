d2d50781b43d6778eccc194b4aa3099604829acc
Mehedi Hasan <mdmehedi23234@gmail.com>
2023-07-22 23:46:36 +0600

    profile info data

--- src/component/core/Shared/RightSidebar.jsx
+++ src/component/core/Shared/RightSidebar.jsx
@@ -2,14 +2,16 @@ import { userContext } from '@/src/Storage/ContextApi';
 import React, { useContext } from 'react';
 import Classic from './Classic/Classic';
 import Flat from './Flat/Flat';
+import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const RightSidebar = () => {
-    const { profileImage, setProfileImage, color, design } = useContext(userContext)
+    const { profileImage, setProfileImage, color, design,logoImage,infos} = useContext(userContext)
     return (
-        <div>
-            <div className='relative w-[363px]'>
-                {design === 'classic' && <Classic color={color} profileImage={profileImage} setProfileImage={setProfileImage}/>}
-                {design === 'flat' && <Flat color={color} profileImage={profileImage} setProfileImage={setProfileImage}/>}
+        <div className='max-h-[70vh] overflow-y-scroll'>
+            <div className='relative w-[363px] '>
+                {design === 'classic' && <Classic color={color} profileImage={profileImage} setProfileImage={setProfileImage} logoImage={logoImage}/>}
+                {design === 'flat' && <Flat color={color} profileImage={profileImage} setProfileImage={setProfileImage} logoImage={logoImage}/>}
+                {infos&& <ProfileInfo infos={infos}/>}
             </div>
         </div>
     );
