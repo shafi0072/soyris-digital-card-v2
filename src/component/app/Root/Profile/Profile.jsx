51259143b86e79e242b6702b3f1dc9fd424717c9
Mehedi Hasan <mdmehedi23234@gmail.com>
2023-07-25 12:55:01 +0600

    profile update

--- src/component/app/Root/Profile/Profile.jsx
+++ src/component/app/Root/Profile/Profile.jsx
@@ -3,16 +3,46 @@ import { profileData, socialInfo } from '@/src/constant/ProfileInfo';
 import React, { useContext } from 'react';
 
 const Profile = ({userData}) => {
+    console.log(userData);
     const { color } = useContext(userContext);
     return (
         <div className='max-w-[500px] w-full mx-auto px-3'>
-            {/* <h2>this is profile page</h2>  */}
+           
+            {userData?.display?.desgine === 'flat' &&
+            <>
             <div className='h-[400px] ' style={{ background: userData?.display?.color }}>
                 <img className='h-[95%] w-full object-cover' src={userData?.display?.ProfileImage} alt="" />
             </div>
             <div className='mt-5'>
                 <img className='md:w-3/4 mx-auto' src={userData?.display?.Logo} alt="" />
             </div>
+            </>
+            }
+            {
+                userData?.display?.design === "classic" && <>
+                <div className=' w-[381px] h-[300px]  relative rounded' style={{ background: userData?.display?.color }}>
+                    <img className='h-full w-full object-cover rounded' src={userData?.display?.ProfileImage} alt="" />
+                    <div className=''>
+                        <div className='absolute  top-[72%]  z-10'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="381" height="88.28" viewBox="0 0 381 88.28">
+  <path id="wave" d="M0,25.9V55.406c70.325,43.351,128.033,45.974,213.535-5.027S340.019,6.009,381,17.739v-7.65C286.9-26.122,210.5,45.427,151.305,63.278S52.111,68.378,0,25.9Z" transform="translate(0 0)" fill="#0e66a0"/>
+</svg>
+                        </div>
+                        <div className='absolute bottom-0 left-0'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="96" height="32.781" viewBox="0 0 96 32.781">
+  <path id="wave-left" d="M0,35.773V68.554H96l-.032-1.475C63.791,67.267,33.386,56.325,0,35.773Z" transform="translate(0 -35.773)" fill="#fff"/>
+</svg>
+                        </div>
+                        <div className='absolute -bottom-[6px] right-0 z-[5]'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="285" height="81.75" viewBox="0 0 285 81.75">
+  <path id="wave-right" d="M300.742,6.114c-30.018-.329-66.667,9.2-121,41.617C136.118,73.767,99.61,86.065,65.025,86.281H65v1.575H350V14.529C334.376,10.055,318.753,6.312,300.742,6.114Z" transform="translate(-65 -6.106)" fill="#fff"/>
+</svg>
+                        </div>
+                     <img className='absolute bottom-3 right-0 z-50 w-[100px]' src={userData?.display?.Logo} alt="" />
+                    </div>
+                </div>
+                </>
+            }
 
             <div className='mt-10 border-4 border-dotted p-3' style={{ borderColor: userData?.display?.color }}>
                 <h2 className='text-xl font-extrabold'>{userData?.profileInfo?.first_name + ' ' + userData?.profileInfo?.last_name}</h2>
