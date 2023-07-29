1002499bae7f6b025c0a7508598842c035881649
Manik <tonysins43@gmail.com>
2023-07-29 17:01:30 +0600

    pull

--- src/component/app/Root/WalletPasses/GoogleCard.jsx
+++ src/component/app/Root/WalletPasses/GoogleCard.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const GoogleCard = () => {
     return (
-        <div>
-            Google Card
+        <div className='bg-[#F7FAFC]  p-12 rounded shadow-md'>
+            <div className='bg-gray-700 w-[350px] text-white rounded p-4'>
+                <div className='flex flex-col items-center justify-center'>
+                    <img className='rounded-full w-32' src="https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/short/linkedin-profile-picture-maker/dummy_image/thumb/004.webp" alt="" />
+                    <h2 className='text-2xl font-semibold mt-2'>Nancy Nancy</h2>
+                </div>
+            </div>
         </div>
     );
 };
