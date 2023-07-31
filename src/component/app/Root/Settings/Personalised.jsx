07ab158ae06f4687106e1ab73471e95795260dc1
Manik <tonysins43@gmail.com>
2023-07-31 20:06:45 +0600

    add something

--- src/component/app/Root/Settings/Personalised.jsx
+++ src/component/app/Root/Settings/Personalised.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react';
 
-const Personalised = () => {
+const Personalised = ({setUrl,handleUrl}) => {
     const inputRef = useRef(null);
     const [isCopied, setIsCopied] = useState(false);
      const handleCopyClick = () => {
@@ -13,14 +13,24 @@ const Personalised = () => {
           }, 1000);
         }
       };
+
+      const permanentUrl = "https://easycard.pro/profile/"; // The permanent URL
+      const [inputValue, setInputValue] = useState('');
+    
+      const handleInputChange = (e) => {
+        setUrl(e.target.value)
+        const inputValue = (e.target.value).slice(29);
+        setInputValue(inputValue)
+
+      };
     
     return (
         <div className='my-8'>
             <h2 className='text-lg font-semibold'>Personalised URL</h2>
             <p className='mt-2 text-md'>Your unique URL is the one below. Click edit to personalise this.</p>
-            <form action="" className='mt-5'>
+            <div action="" className='mt-5'>
                 <div className="flex gap-3 items-center">
-                    <input ref={inputRef} type="text" className='px-2 py-1 shadow-lg rounded w-[306px] text-[17px] font-medium' placeholder='https://easycards.pro/eCzsUa' name="work" id="" />
+                    <input onChange={handleInputChange} value={permanentUrl + inputValue}   ref={inputRef} type="text" className='px-2 py-1 shadow-lg rounded w-[306px] text-[17px] font-medium'  name="url" id="" />
                     <span className='cursor-pointer' onClick={handleCopyClick} >
                         <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 23 23">
                             <path id="copy-svgrepo-com_1_" data-name="copy-svgrepo-com (1)" d="M19.917,15.833h1.75A2.333,2.333,0,0,0,24,13.5V5.333A2.333,2.333,0,0,0,21.667,3H13.5a2.333,2.333,0,0,0-2.333,2.333v1.75M5.333,11.167H13.5A2.333,2.333,0,0,1,15.833,13.5v8.167A2.333,2.333,0,0,1,13.5,24H5.333A2.333,2.333,0,0,1,3,21.667V13.5A2.333,2.333,0,0,1,5.333,11.167Z" transform="translate(-2 -2)" fill="none" stroke="#0277b5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
@@ -28,8 +38,8 @@ const Personalised = () => {
                     </span>
                     {isCopied && <p style={{ color: 'green' }}>Copied!</p>}
                 </div>
-            </form>
-            <button className='mt-4 bg-[#0277B5] text-sm text-white px-3 py-1 rounded'>EDIT</button>
+            </div>
+            <button onClick={handleUrl} className='mt-4 bg-[#0277B5] text-sm text-white px-3 py-1 rounded'>EDIT</button>
         </div>
     );
 };
--- src/component/app/Root/Settings/Settings.jsx
+++ src/component/app/Root/Settings/Settings.jsx
@@ -1,20 +1,77 @@
-import React from 'react';
-import CardName from './CardName';
-import Personalised from './Personalised';
-import CardStatus from './CardStatus';
-import DuplicateCard from './DuplicateCard';
-import DeleteCard from './DeleteCard';
+import React, { useState } from "react";
+import CardName from "./CardName";
+import Personalised from "./Personalised";
+import CardStatus from "./CardStatus";
+import DuplicateCard from "./DuplicateCard";
+import DeleteCard from "./DeleteCard";
+import { baseUrl } from "@/src/config/Server";
+import { data } from "autoprefixer";
+import Swal from "sweetalert2";
 
 const Settings = () => {
-    return (
-        <div>
-            <CardName/>
-            <Personalised/>
-            <CardStatus/>
-            <DuplicateCard/>
-            <DeleteCard/>
-        </div>
-    );
+  const [cardName, setCardName] = useState("");
+  const [url, setUrl] = useState("");
+  console.log(cardName, url);
+
+  const handleCardNameOnChange = (event) => {
+    const cardName = event.target.value;
+    setCardName(cardName);
+  };
+
+  // handle cardName
+  const handleCardName = () => {
+    fetch(`${baseUrl}/add-user/profile/setting/`, {
+      method: "PUT",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ cardName: cardName }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          Swal.fire({
+            position: "top-center",
+            icon: "success",
+            title: "Successfully Updated",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+
+  // handle url
+  const handleUrl = () => {
+    fetch(`${baseUrl}/add-user`, {
+      method: "PUT",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ url: url }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          Swal.fire({
+            position: "top-center",
+            icon: "success",
+            title: "Successfully Updated",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+
+  return (
+    <div>
+      <CardName
+        handleCardNameOnChange={handleCardNameOnChange}
+        handleCardName={handleCardName}
+      />
+      <Personalised setUrl={setUrl} handleUrl={handleUrl} />
+      <CardStatus />
+      <DuplicateCard />
+      <DeleteCard />
+    </div>
+  );
 };
 
 export default Settings;
