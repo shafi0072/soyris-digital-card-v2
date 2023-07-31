20d484e11470e367c8875d4b8fcbaa04d9adaccd
shafi0072 <safi29317@gmail.com>
2023-07-31 23:55:37 +0600

    s

--- src/component/app/Root/Analytics/Analytics.jsx
+++ src/component/app/Root/Analytics/Analytics.jsx
@@ -5,7 +5,7 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateRangePicker } from "@mui/x-date-pickers-pro/DateRangePicker";
 import { SingleInputDateRangeField } from "@mui/x-date-pickers-pro/SingleInputDateRangeField";
-import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+
 
 // select
 import {
@@ -29,9 +29,9 @@ const Analytics = () => {
         <div className="flex gap-4 items-center justify-end">
           <div className="w-80 ">
             <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DemoContainer components={["SingleInputDateRangeField"]}>
+             
                 <DateRangePicker slots={{ field: SingleInputDateRangeField }} />
-              </DemoContainer>
+             
             </LocalizationProvider>
           </div>
           <div>
--- src/component/app/Root/Analytics/CardViewPieChart.jsx
+++ src/component/app/Root/Analytics/CardViewPieChart.jsx
@@ -1,27 +1,9 @@
-import React from "react";
-import { PieChart } from "@mui/x-charts/PieChart";
+import React from 'react';
 
 const CardViewPieChart = () => {
   return (
-    <div className="bg-white shadow-lg rounded">
-      <h2 className="border-b text-center py-3 text-xl">Card Views By Device</h2>
-    <div className="my-6">
-    <PieChart
-        series={[
-          {
-            data: [
-              { id: 0, value: 10, label: "series A" },
-              { id: 1, value: 15, label: "series B" },
-              { id: 2, value: 20, label: "series C" },
-              { id: 3, value: 25, label: "series D" },
-              { id: 4, value: 30, label: "series E" },
-            ],
-          },
-        ]}
-        width={470}
-        height={300}
-      />
-    </div>
+    <div>
+      
     </div>
   );
 };
