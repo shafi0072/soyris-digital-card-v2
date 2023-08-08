b24741700c01a9bf326a50198db6453013df4662
shafi0072 <safi29317@gmail.com>
2023-08-09 00:06:23 +0600

    s

--- src/component/app/Root/Design/ProfileImage.jsx
+++ src/component/app/Root/Design/ProfileImage.jsx
@@ -3,6 +3,8 @@ import Cropper from 'react-cropper';
 import CloseIcon from '@mui/icons-material/Close';
 import AddIcon from '@mui/icons-material/Add';
 import { userContext } from '@/src/Storage/ContextApi';
+import Compress from 'canvas-compress';
+
 const ProfileImage = () => {
   const [selectedImage, setSelectedImage] = useState(null);
   const [croppedImage, setCroppedImage] = useState(null);
@@ -17,10 +19,15 @@ const ProfileImage = () => {
   };
 
   const handleCrop = () => {
+    if (typeof window === 'undefined') {
+      // Server-side rendering, do nothing
+      return null;
+    }
+    
     if (!cropperRef.current || typeof cropperRef.current.cropper.getCroppedCanvas() === 'undefined') {
       return;
     }
-
+  
     const croppedCanvas = cropperRef.current.cropper.getCroppedCanvas({
       minWidth: 200,
       minHeight: 200,
@@ -28,10 +35,28 @@ const ProfileImage = () => {
       maxHeight: 800,
       fillColor: '#fff',
     });
-
-    setCroppedImage(croppedCanvas.toDataURL());
-    setProfileImage(croppedCanvas.toDataURL())
+  
+    // Convert canvas to blob with compressed image data
+    croppedCanvas.toBlob(
+      (blob) => {
+        if (blob) {
+          // Convert blob to base64
+          const reader = new FileReader();
+          reader.onload = () => {
+            const base64String = reader.result;
+            setCroppedImage(base64String);
+            setProfileImage(base64String);
+          };
+          reader.readAsDataURL(blob);
+        }
+      },
+      'image/jpeg', // Use 'image/webp' for WebP format
+      0.8 // Adjust the compression quality as needed
+    );
   };
+  
+  // ...
+  
 
   const handleAspectRatioChange = (e) => {
     setCropAspectRatio(parseFloat(e.target.value));
