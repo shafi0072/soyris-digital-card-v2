a925cec7c59ba2f387ca9f752837924a6eee0295
shafi0072 <safi29317@gmail.com>
2023-08-09 00:33:46 +0600

    s

--- src/config/Server.js
+++ src/config/Server.js
@@ -1,2 +1,2 @@
-// export const baseUrl = 'https://business-card-backend-2.vercel.app'
-export const baseUrl = 'http://localhost:5050'
+export const baseUrl = 'https://business-card-backend-2.vercel.app'
+// export const baseUrl = 'http://localhost:5050'
