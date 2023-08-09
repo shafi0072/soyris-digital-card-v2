60e39d4b617ac0ff6ed2fa020da667884ce7c62a
Manik <tonysins43@gmail.com>
2023-08-10 01:27:08 +0600

    s

--- src/component/app/Root/Analytics/CardViewPieChart.jsx
+++ src/component/app/Root/Analytics/CardViewPieChart.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import { userContext } from "@/src/Storage/ContextApi";
+import React, { useContext } from "react";
 import { PureComponent } from "react";
+import { useTouchSensor } from "react-beautiful-dnd";
 import {
   PieChart,
   Pie,
@@ -10,18 +12,41 @@ import {
 } from "recharts";
 
 const CardViewPieChart = () => {
-  const data = [
-    { name: "Group A", value: 400 },
-    { name: "Group B", value: 300 },
-    { name: "Group C", value: 300 },
-    { name: "Group D", value: 200 },
-  ];
+  const { userCardData } = useContext(userContext);
+  console.log(userCardData.anylatics);
+
+
+  const deviceViewsMap = {};
+
+  userCardData?.anylatics?.forEach((item) => {
+    const device = item.device;
+    const viewCount = item.view || 0; 
+
+    if (!deviceViewsMap[device]) {
+      deviceViewsMap[device] = 0;
+    }
+
+    deviceViewsMap[device] += viewCount;
+  });
+
+  const resultArray = Object.keys(deviceViewsMap).map((device) => {
+    return { device, totalViews: deviceViewsMap[device] };
+  });
+
+  const chartData = resultArray?.map((item, index) => ({
+    name: item.device,
+    value: item.totalViews,
+  }));
+  // console.log(chartData);
   const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
   return (
-    <div className="shadow-lg" style={{ display: "flex", flexDirection: "row" }}>
+    <div
+      className="shadow-lg"
+      style={{ display: "flex", flexDirection: "row" }}
+    >
       <PieChart width={400} height={400}>
         <Pie
-          data={data}
+          data={chartData}
           cx={120}
           cy={200}
           innerRadius={60}
@@ -30,7 +55,7 @@ const CardViewPieChart = () => {
           paddingAngle={5}
           dataKey="value"
         >
-          {data.map((entry, index) => (
+          {chartData?.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
           ))}
         </Pie>
