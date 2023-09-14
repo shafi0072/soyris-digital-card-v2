222b9f1d7ceb0e3dfa250f004a51557a50b52f4f
mehedidev <mdmehedi23234@gmail.com>
2023-09-14 10:53:10 +0600

    fyrhfy

--- src/component/core/Shared/Flat/Flat.jsx
+++ src/component/core/Shared/Flat/Flat.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
 const Flat = ({ color, profileImage, logoImage }) => {
+  console.log({logoImage});
   return (
     <div className='bg-white mx-4' >
-      <div className=' w-[100%] h-[300px]  relative ' >
-        {profileImage ? <img className='h-[100%] w-full object-cover rounded-t-2xl' src={profileImage ? profileImage : ""} alt="" />: <div className='w-[100%]  h-[300px]'  style={{background:color}}></div>}
-        <div className='absolute top-[95%] h-5 w-full rounded-b-md' style={{borderBottom: `10px solid ${color}` }}></div>
+      <div className=' w-[100%]  relative border-b-[12px] rounded-md ' style={{borderColor: color}}>
+        {profileImage ? <img className=' object-cover rounded-t-2xl' src={profileImage ? profileImage : ""} alt="" />: <div className='w-[100%]  h-[300px]'  style={{background:color}}></div>}
+        {/* <div className='absolute top-[95%] h-5 w-full rounded-b-md' style={{borderBottom: `10px solid ${color}` }}></div> */}
       </div>
       <div className='w-[100%] mt-5'>
-        {logoImage && <img className='w-[200px] h-[100px]   object-fill rounded' src={logoImage ? logoImage : ""} alt="" />}
+        {logoImage !== 'null' &&   <img className='w-[100%] h-[60px] object-fill rounded' src={logoImage } alt="" />}
       </div>
 
     </div>
