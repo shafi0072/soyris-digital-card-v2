0225ff7d7e6c0fb5238bbfe29d89042e338004ff
shafi0072 <safi29317@gmail.com>
2024-05-25 22:35:08 +0600

    s

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -11,17 +11,6 @@ const firebaseConfig = {
   measurementId: "G-VDKWT4XMXB"
 };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDCWgaFZcUsFBgd6FIPjh2jHxacdhZDlEg",
-//   authDomain: "easy-cards-pro.firebaseapp.com",
-//   projectId: "easy-cards-pro",
-//   storageBucket: "easy-cards-pro.appspot.com",
-//   messagingSenderId: "641301914906",
-//   appId: "1:641301914906:web:14a3217e467625f5a6b8a3",
-//   measurementId: "G-42406S8QYB"
-// };
-// Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
