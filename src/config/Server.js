b7e7f95d24d0e7f5ab95a1c92a3eecc661dd77db
shafi0072 <safi29317@gmail.com>
2024-06-12 14:46:07 +0600

    url changed

--- src/config/Server.js
+++ src/config/Server.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'https://easy-8dfcbe39d61b.herokuapp.com'
-// export const baseUrl = 'http://localhost:5050'
+// export const baseUrl = 'https://easy-8dfcbe39d61b.herokuapp.com'
+export const baseUrl = 'http://52.6.119.16:5000'
