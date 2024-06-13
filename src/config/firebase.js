47d8ddd61354dadac17adee6ec39bb500af58ed7
Md Abdullah Al Safi <67519541+shafi0072@users.noreply.github.com>
2024-06-13 16:44:42 +0600

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDCWgaFZcUsFBgd6FIPjh2jHxacdhZDlEg",
-  authDomain: "easy-cards-pro.firebaseapp.com",
-  projectId: "easy-cards-pro",
-  storageBucket: "easy-cards-pro.appspot.com",
-  messagingSenderId: "641301914906",
-  appId: "1:641301914906:web:14a3217e467625f5a6b8a3",
-  measurementId: "G-42406S8QYB"
+  apiKey: "AIzaSyAZ3SvBNrKp8xsbxnnUZT5kYRdzMXibCUA",
+  authDomain: "easycards-da879.firebaseapp.com",
+  projectId: "easycards-da879",
+  storageBucket: "easycards-da879.appspot.com",
+  messagingSenderId: "963554179240",
+  appId: "1:963554179240:web:1f068ac29b4edebb722a9d",
+  measurementId: "G-VDKWT4XMXB"
 };
 
 const app = initializeApp(firebaseConfig);
